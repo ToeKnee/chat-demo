afd989292e2c403d66339198ea808fe02338325a
Anthony Batchelor <tony@ynottony.net>
2017-03-11 11:36:27 +0000

    Setup tox and setup.py for running tests
    Test against py27 and py35.
    Only using sqlite3 for now, but can add other DBs in here too.

---
+++ backend/chat/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='Chat',
+    version='0.1',
+    description='Simple Chat Wall',
+    author='Anthony Batchelor',
+    author_email='tony@ynottony.net',
+    url='https://ten.ynottony.net/',
+    packages=['chat', 'users'],
+)
