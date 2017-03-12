9fce176efc112e03da6ba507da6f63c9d0615ae7
Anthony Batchelor <tony@ynottony.net>
2017-03-12 12:25:44 +0000

    Add Message serializer

---
+++ backend/chat/wall/tests/api/test_serializers.py
@@ -0,0 +1,19 @@
+from __future__ import unicode_literals
+
+from django.test import TestCase
+
+from wall.api.serializers import MessageSerializer
+from wall.factories import MessageFactory
+
+
+class MessageSerializerTest(TestCase):
+    def test_read_fields(self):
+        message = MessageFactory.build()
+        serializer = MessageSerializer(message)
+
+        self.assertEqual(serializer.data["message"], message.message)
+        self.assertEqual(serializer.data["user"], message.user.username)
+        self.assertEqual(
+            serializer.data["timestamp"],
+            message.timestamp.strftime("%Y-%m-%dT%H:%M:%S.%fZ")
+        )
