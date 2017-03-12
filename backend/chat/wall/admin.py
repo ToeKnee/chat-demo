30c2333cb5444501269deb8326476ecc0f6a84fa
Anthony Batchelor <tony@ynottony.net>
2017-03-12 11:27:17 +0000

    Add Message model to wall app

--- backend/chat/wall/admin.py
+++ backend/chat/wall/admin.py
@@ -1,3 +1,12 @@
+from __future__ import unicode_literals
+
 from django.contrib import admin
 
-# Register your models here.
+from .models import Message
+
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    date_hierarchy = 'timestamp'
+    list_display = ('message', 'user', 'timestamp',)
+    search_fields = ('message', 'user__username', 'user__email',)
--- backend/chat/wall/models.py
+++ backend/chat/wall/models.py
@@ -1,3 +1,22 @@
-from django.db import models
+from __future__ import unicode_literals
 
-# Create your models here.
+from django.conf import settings
+from django.db import models
+from django.utils import timezone
+from django.utils.encoding import python_2_unicode_compatible
+from django.utils.translation import ugettext_lazy as _
+
+
+@python_2_unicode_compatible
+class Message(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        verbose_name=_("User"),
+        related_name="messages",
+        on_delete=models.CASCADE
+    )
+    message = models.TextField(_("Message"))
+    timestamp = models.DateTimeField(_("Timestamp"), default=timezone.now)
+
+    def __str__(self):
+        return self.message
